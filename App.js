5217334b289ee6f15974f63f536c2c1835a90309
mfazal <mfazal@gmail.com>
2020-10-29 02:13:46 +0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {useDeviceOrientation, useDimensions} from '@react-native-community/hooks';
+
 
 export default function App() {
+
+  const {landscape} = useDeviceOrientation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+      style= {{
+        backgroundColor: "dodgerblue",
+        width: "100%",
+        height: landscape ? "100%" : "30%"
+      }}
+      ></View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'red', 
+    width: "100%",
+    height: "30%",
+
   },
 });
