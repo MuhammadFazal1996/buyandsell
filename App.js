7a29d696b381352fd747144ac8fb0e321059e2d1
mfazal <mfazal@gmail.com>
2020-11-16 19:45:06 +0500

    message screen working on list

--- App.js
+++ App.js
@@ -8,12 +8,13 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 
 
 export default function App() {
 
-  return <ListingDetailsScreen />;
+  return <MessagesScreen />;
   
 }
 
